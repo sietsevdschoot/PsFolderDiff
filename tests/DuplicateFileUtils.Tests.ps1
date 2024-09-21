6e988aff4af6137be114c2eca5befae43c062d37
Sietse <sietsevdschoot@gmail.com>
2024-09-21 21:48:09 +0200

    Added Pester extensions and test

--- tests/DuplicateFileUtils.Tests.ps1
+++ tests/DuplicateFileUtils.Tests.ps1
@@ -2,31 +2,31 @@ using module '..\src\DuplicateFileUtils.psm1'
 
 Describe "DuplicateFileUtils" {
 
-    It "Get-Duplicates: List all folders containing duplicates" -Skip {
+    It "Get-Duplicates: Lists all duplicate files" {
 
-        $fileContent = [Guid]::NewGuid()
+        $fileContent1 = [Guid]::NewGuid()
+        $fileContent2 = [Guid]::NewGuid()
 
-        1..3 | ForEach-Object { New-Item -ItemType File "$TestDrive\Folder1\$_.txt" -Value $fileContent -Force }
-        4..6 | ForEach-Object { New-Item -ItemType File "$TestDrive\Folder2\$_.txt" -Value $fileContent -Force }
+        10..13 | ForEach-Object { New-Item -ItemType File "$TestDrive\Folder1\$_.txt" -Value ($_ % 2 -eq 0 ? $fileContent1 : $fileContent2) -Force }
+        20..23 | ForEach-Object { New-Item -ItemType File "$TestDrive\Folder2\$_.txt" -Value ($_ % 2 -eq 0 ? $fileContent1 : $fileContent2) -Force }
+        30..32 | ForEach-Object { New-Item -ItemType File "$TestDrive\Folder3\$_.txt" -Value ($_ % 2 -eq 0 ? $fileContent1 : $fileContent2) -Force }
 
         $fileHashTable = GetFileHashTable $TestDrive
 
         $actual = Get-Duplicates $fileHashTable
         
-        $expected = @(
-            "$TestDrive\RootFolder",
-            "$TestDrive\RootFolder\Folder1",
-            "$TestDrive\RootFolder\Folder1\SubFolder1",
-            "$TestDrive\RootFolder\Folder1\SubFolder1\Sub",
-            "$TestDrive\RootFolder\Folder1\SubFolder2",
-            "$TestDrive\RootFolder\Folder1\SubFolder2\Sub")
-
-        $actual | Should -Be $expected
+        $actual | Should -BeEquivalentTo @(
+            @{ Keep="$TestDrive\Folder1\10.txt"; Duplicates=@("$TestDrive\Folder2\20.txt", "$TestDrive\Folder3\30.txt") },
+            @{ Keep="$TestDrive\Folder1\11.txt"; Duplicates=@("$TestDrive\Folder2\21.txt", "$TestDrive\Folder3\31.txt") },
+            @{ Keep="$TestDrive\Folder1\12.txt"; Duplicates=@("$TestDrive\Folder2\22.txt", "$TestDrive\Folder3\32.txt") },
+            @{ Keep="$TestDrive\Folder1\13.txt"; Duplicates=@("$TestDrive\Folder2\23.txt" ) }
+        ) 
     }
 
     BeforeEach {
         
         1..3 | ForEach-Object { New-Item -ItemType File Testdrive:\MyFolder\$_.txt -Value "My Test Value $_" -Force  }
+
         Set-Location $TestDrive
     }
 
@@ -40,7 +40,13 @@ Describe "DuplicateFileUtils" {
 
         & ".\Reload.ps1"
 
-        $originalLocation = Get-Location 
+        $originalLocation = Get-Location
+
+        Import-Module $PSScriptRoot\Extensions\PesterExtensions.psm1 -Force
+    
+        Add-ShouldOperator -Name BeEquivalentTo -Test $function:BeEquivalentTo -SupportsArrayInput
+        Add-ShouldOperator -Name ContainEquivalentOf -Test $function:ContainEquivalentOf -SupportsArrayInput
+    
     }
 
     AfterAll {
