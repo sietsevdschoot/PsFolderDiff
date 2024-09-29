1b1aab7ee87008d5a1d3015c5a27fbdf4703b8dc
Sietse <sietsevdschoot@gmail.com>
2024-09-29 19:48:54 +0200

    Added ExcludePatternTests

--- src/FolderDiff/FileHashLookup/Services/FileCollector.cs
+++ src/FolderDiff/FileHashLookup/Services/FileCollector.cs
@@ -7,13 +7,15 @@ namespace PsFolderDiff.FileHashLookup.Services;
 public class FileCollector
 {
     private readonly IFileSystem _fileSystem;
-    private readonly List<string> _includePatterns;
+    private readonly List<(string Directory, string RelativePattern)> _includePatterns;
+    private readonly List<string> _excludePatterns;
 
     public FileCollector(IFileSystem fileSystem)
     {
         _fileSystem = fileSystem;
 
-        _includePatterns = new List<string>();
+        _includePatterns = new List<(string Directory, string RelativePattern)>();
+        _excludePatterns = new List<string>();
     }
 
     public FileCollector AddIncludeFolder(string path)
@@ -25,18 +27,32 @@ public class FileCollector
 
     public FileCollector AddIncludePattern(string includePattern)
     {
-        ArgumentException.ThrowIfNullOrEmpty(includePattern, nameof(includePattern));
+        EnsurePatternIsValid(includePattern);
 
-        var pattern = ParsePattern(includePattern);
-
-        if (!_fileSystem.Directory.Exists(pattern.Directory))
-        {
-            throw new ArgumentException($"Folder '{pattern.Directory}' does not exist");
-        }
-
-        _includePatterns.Add(includePattern);
+        _includePatterns.Add(ParsePattern(includePattern));
 
         return this;
+    }
+
+    public FileCollector AddExcludePattern(string excludePattern)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(excludePattern, nameof(excludePattern));
+
+        _excludePatterns.Add(excludePattern);
+
+        return this;
+    }
+
+    private void EnsurePatternIsValid(string pattern)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(pattern, nameof(pattern));
+
+        var parsedPattern = ParsePattern(pattern);
+
+        if (!_fileSystem.Directory.Exists(parsedPattern.Directory))
+        {
+            throw new ArgumentException($"Folder '{parsedPattern.Directory}' does not exist");
+        }
     }
 
     public List<IFileInfo> GetFiles()
@@ -45,18 +61,21 @@ public class FileCollector
 
         foreach (var includePattern in _includePatterns)
         {
-            var pattern = ParsePattern(includePattern);
-            
-            var matcher = new Matcher(StringComparison.OrdinalIgnoreCase)
-                .AddInclude(pattern.RelativePattern);
+            var matcher = new Matcher(StringComparison.OrdinalIgnoreCase).AddInclude(includePattern.RelativePattern);
 
-            var result = matcher.Execute(_fileSystem, pattern.Directory);
+            foreach (var excludePattern in _excludePatterns)
+            {
+                matcher.AddExclude(excludePattern);
+            }
+
+            var result = matcher.Execute(_fileSystem, includePattern.Directory);
 
             if (result.HasMatches)
             {
                 foreach (var filePatternMatch in result.Files)
                 {
-                    var foundFile = _fileSystem.FileInfo.New(_fileSystem.Path.Combine(pattern.Directory, filePatternMatch.Path));
+                    var foundFile = _fileSystem.FileInfo.New(
+                        _fileSystem.Path.Combine(includePattern.Directory, filePatternMatch.Path));
 
                     collectedFiles.Add(foundFile);
                 }
--- src/FolderDiff/tests/UnitTests/Services/FileCollectorTests.cs
+++ src/FolderDiff/tests/UnitTests/Services/FileCollectorTests.cs
@@ -1,11 +1,25 @@
 ﻿using FluentAssertions;
 using PsFolderDiff.FileHashLookup.Services;
+using System.IO;
 using Xunit;
 
 namespace PsFolderDiff.FileHashLookup.UnitTests.Services;
 
 public class FileCollectorTests
 {
+    [Fact]
+    public void AddIncludeFolder_Adding_NonExisting_Folder_Throws()
+    {
+        // Arrange 
+        var fixture = new FileCollectorTestFixture();
+
+        // Act
+        var act = () => fixture.AddIncludeFolder("NonExistingFolder");
+
+        // Assert
+        act.Should().Throw<ArgumentException>();
+    }
+
     [Fact]
     public void AddIncludeFolder_Adds_Folder_And_Collects_Files_Recursively()
     {
@@ -24,19 +38,6 @@ public class FileCollectorTests
         fixture.AssertContainsFileNames([ 1, 2, 3, 4]);
     }
 
-    [Fact]
-    public void AddIncludeFolder_Adding_NonExisting_Folder_Throws()
-    {
-        // Arrange 
-        var fixture = new FileCollectorTestFixture();
-
-        // Act
-        var act = () => fixture.AddIncludeFolder("NonExistingFolder");
-
-        // Assert
-        act.Should().Throw<ArgumentException>();
-    }
-
     [Fact] 
     public void AddIncludePattern_Can_use_file_glob()
     {
@@ -52,6 +53,45 @@ public class FileCollectorTests
 
         // Assert
         fixture.AssertContainsFileNames([1, 4]);
+    }
+
+    [Fact]
+    public void AddExcludePattern_ExcludesFolderInCollectedFileResults()
+    {
+        // Arrange 
+        var fixture = new FileCollectorTestFixture();
+        fixture.WithNewFile(@"Folder1\1.txt");
+        fixture.WithNewFile(@"Folder1\2.txt");
+        fixture.WithNewFile(@"Folder1\Sub\3.txt");
+        fixture.WithNewFile(@"Folder1\Sub\Sub\4.txt");
+        fixture.WithNewFile(@"Folder1\5.txt");
+        fixture.WithNewFile(@"Folder2\6.txt");
+
+        // Act
+        fixture.AddIncludeFolder(@"\Folder1\");
+        fixture.AddExcludePattern("\\**\\Sub\\**\\*");
+
+        // Assert
+        fixture.AssertContainsFileNames([1, 2, 5]);
+    }
+
+    [Fact]
+    public void AddExcludePattern_CanExcludesPatternInCollectedFilesResults()
+    {
+        // Arrange 
+        var fixture = new FileCollectorTestFixture();
+        fixture.WithNewFile(@"Folder1\1.txt");
+        fixture.WithNewFile(@"Folder1\2.doc");
+        fixture.WithNewFile(@"Folder1\Sub\3.doc");
+        fixture.WithNewFile(@"Folder1\Sub\Sub\4.doc");
+        fixture.WithNewFile(@"Folder1\5.txt");
+
+        // Act
+        fixture.AddIncludeFolder(@"Folder1\");
+        fixture.AddExcludePattern($"/**/*.doc");
+
+        // Assert
+        fixture.AssertContainsFileNames([1, 5]);
     }
 
     private class FileCollectorTestFixture : FileHashTestFixture
@@ -83,11 +123,21 @@ public class FileCollectorTests
             return this;
         }
 
+        public FileCollectorTestFixture AddExcludePattern(string pattern)
+        {
+            _sut.AddExcludePattern(pattern);
+
+            return this;
+        }
+
         public void AssertContainsFileNames(params int[] expected)
         {
             var files = _sut.GetFiles();
 
-            var actual = files.Select(x => Convert.ToInt32(Path.GetFileNameWithoutExtension(x.FullName))).ToList();
+            var actual = files
+                .Select(x => Convert.ToInt32(Path.GetFileNameWithoutExtension(x.FullName)))
+                .OrderBy(x => x)
+                .ToList();
 
             actual.Should().BeEquivalentTo(expected);
         }
